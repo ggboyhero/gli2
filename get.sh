7488c9c11d4c2ec7e6e77d68a32b610351ad8016
ggboyhero <130449163+ggboyhero@users.noreply.github.com>
2023-05-14 23:13:19 -0800

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 2m
+while [ "a" = "a" ]
+do
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  curl https://zinc-axiomatic-vacation.glitch.mes
+  sleep 4m
+done
---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# set val
+python3 /app/self-ping.py &
+
+AUUID=e2c3529b-54ca-43e4-99e8-dab623388aa4
+ParameterSSENCYPT=chacha20-ietf-poly1305
+CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
+
+# download execution
+wget "https://github.com/ctrlmelive/cccccc/raw/main/caddy.zip" -O caddy.zip
+wget "https://github.com/ctrlmelive/cccccc/raw/main/Xray-linux-64.zip" -O xray-linux-64.zip
+
+unzip -o caddy.zip && rm -rf caddy.zip
+unzip -o xray-linux-64.zip && rm -rf xray-linux-64.zip
+chmod +x caddy xray
+
+# set caddy
+mkdir -p etc/caddy/ usr/share/caddy
+echo -e "User-agent: *\nDisallow: /" > usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O usr/share/caddy/index.html && unzip -qo usr/share/caddy/index.html -d usr/share/caddy/ && mv usr/share/caddy/*/* usr/share/caddy/
+
+
+# set config file
+cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
+cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
+
+
+# start service
+./xray -config xray.json &
+./caddy run --config etc/caddy/Caddyfile --adapter caddyfile
+
+
+sleep 20
+echo "start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+nohup bash get.sh &>/dev/null &
+echo "OK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
